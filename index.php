16076a1bf6dc13db3b391c5108518678edd879de
Benjamin Lu <benlumia007@gmail.com>
2019-02-14 16:13:28 -0800

    updated for Auspicious

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Initiator (index.php)
+ * Auspicious (index.php)
  *
  * The (index.php) template file is very flexible, it can be used to include all the references for the header, content,
  * aside, footer, and other pages created in WordPress. The (index.php) can also be divided into modular template files,
  * each taking on part of the workload. If you wish to not provide any other template files, the WordPress hierarchy may
  * have default template files or functions to perform their jobs.
  *
- * @package     Initiator Starter Theme
- * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @package     Auspicious
+ * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  * @author      Benjamin Lu (https://getbenonit.com)
  */
@@ -16,10 +16,8 @@
 ?>
 <?php get_header(); ?>
 	<section id="main" class="site-main">
-		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
-			<div class="content-area">
-				<?php Benlumia007\Backdrop\MainQuery\display( 'content-post-format' ); ?>
-			</div>
+		<div class="content-area grid">
+			<?php Benlumia007\Backdrop\MainQuery\display( 'content-post-format' ); ?>
 		</div>
 	</section>
 <?php get_footer(); ?>
