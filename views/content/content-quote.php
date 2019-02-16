96315076f09d53a4cddfd4b3a3fcc52f005b976b
Benjamin Lu <benlumia007@gmail.com>
2019-02-16 00:03:46 -0800

    added content-quote.php

---
+++ views/content/content-quote.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Auspicious (content-page.php)
+ *
+ * @package     Auspicious
+ * @copyright   Copyright (C) 2018. Benjamin Lu
+ * @license     GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
+ * @author      Benjamin Lu (https://getbenonit.com)
+ * ************************************************************************************************************************
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php Benlumia007\Backdrop\Entry\display( 'entry-post-thumbnail' ); ?>
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'auspicious' ),
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'auspicious' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">,</span> ',
+				)
+			);
+			?>
+	</div>
+</article>
